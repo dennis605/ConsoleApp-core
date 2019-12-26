a9e2c084c724a644b3acbc566f10c6d543ded155
Dennis Schönig <dennis605@web.de>
2019-12-26 11:00:30 +0100

    Projektdateien hinzufügen.

---
+++ ConsoleApp-core/Event.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_core
+{
+    class Event
+    {
+        private int eventId { get; set; }
+        private string eventName { get; set; }
+
+        private DateTime eventDatum { get; set; }
+        private DateTime eventBeginn { get; set; }
+        public DateTime EventEnd { get; set; }
+        public int EventVorlauf { get; set; }
+        public string EventOrt { get; set; }
+        public string EventBeschreibung { get; set; }
+        public string EventInfo { get; set; }
+
+
+        public ICollection<Person> Persons { get; set; }
+    }
+}
---
+++ ConsoleApp-core/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_core
+{
+    class Person
+    {
+        public Person()
+        {
+            this.Events = new HashSet<Event>();
+        }
+        public int PersonId { get; set; }
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public string Rolle { get; set; }
+
+        public ICollection<Event> Events { get; set; }
+    }
+}
